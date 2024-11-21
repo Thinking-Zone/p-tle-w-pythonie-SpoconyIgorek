5e50a98c80920bea4c7b3124b97056d303b96510
SpoconyIgorek <jakub.plocki@thinkingzone.pl>
2024-11-21 09:39:45 +0100

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,4 +1,4 @@
-uma = 0  # Zmienna do przechowywania sumy
+suma = 0  # Zmienna do przechowywania sumy
  
 for i in range(1, 101):  # Przechodzimy przez liczby od 1 do 100
     if i % 2 != 0:  # Sprawdzamy, czy liczba jest nieparzysta (reszta z dzielenia przez 2 różna od 0)
